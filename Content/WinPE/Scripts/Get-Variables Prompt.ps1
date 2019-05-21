2ad6ef963b323d1c4a810904c5ffc0d471dc41c8
David Segura <david@segura.org>
2019-05-20 23:30:13 -0500

    19.5.20.0

--- Content/WinPE/Scripts/Get-Variables Prompt.ps1
+++
@@ -1,6 +0,0 @@
-#======================================================================================
-#   Displays Variables and Prompts for Enter
-#   Useful for Debugging
-#======================================================================================
-Get-Variable | Select-Object -Property Name, Value | Format-Table
-[void](Read-Host 'Press Enter to continue')
